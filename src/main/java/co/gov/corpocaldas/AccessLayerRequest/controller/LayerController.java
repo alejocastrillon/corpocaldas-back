bdb82d0f310de12f6421940f55655acdcf3a522b
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2021-05-04 20:50:19 -0500

    Adding 'api' prefix to endpoints

--- src/main/java/co/gov/corpocaldas/AccessLayerRequest/controller/LayerController.java
+++ src/main/java/co/gov/corpocaldas/AccessLayerRequest/controller/LayerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/layers")
+@RequestMapping("/api/layers")
 @Api
 public class LayerController {
 

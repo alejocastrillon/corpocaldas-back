02152ba3a2c6e25ddb0b006607be757e75995cc6
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2021-05-12 00:35:59 -0500

    Añadiendo paginación a peticiones de acceso

--- src/main/java/co/gov/corpocaldas/AccessLayerRequest/repository/AccessRequestRepository.java
+++ src/main/java/co/gov/corpocaldas/AccessLayerRequest/repository/AccessRequestRepository.java
@@ -18,7 +18,7 @@ public interface AccessRequestRepository extends JpaRepository<AccessRequest, In
             " AND (:company IS NULL OR a.company LIKE %:company%) AND (:email IS NULL OR a.email LIKE %:email%)" +
             " AND (:layername IS NULL OR a.layer.name LIKE %:layername%)" +
             " AND (:layeraccessgranted IS NULL OR a.layer.accessGranted = :layeraccessgranted)" +
-            " AND (:approved IS NULL OR a.approved = :approved)")
+            " AND a.approved = :approved")
     Page<AccessRequest> getAll(@Param("name") String name, @Param("company") String company,
                                @Param("email") String email, @Param("layername") String layername,
                                @Param("layeraccessgranted") Integer layeraccessgranted,

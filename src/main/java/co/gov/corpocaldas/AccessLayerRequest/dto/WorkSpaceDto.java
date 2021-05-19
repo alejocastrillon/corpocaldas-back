c18f05b1f67ea7f938d79fe0b051778bcd44e707
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2021-05-18 23:51:24 -0500

    Corregidos errores de filtrado

--- src/main/java/co/gov/corpocaldas/AccessLayerRequest/dto/WorkSpaceDto.java
+++ src/main/java/co/gov/corpocaldas/AccessLayerRequest/dto/WorkSpaceDto.java
@@ -18,12 +18,20 @@ public class WorkSpaceDto {
     private String name;
     @JsonBackReference
     private WorkSpaceDto parent;
-    private long idParent;
+    private Long idParent;
     private String nameParent;
     private List<WorkSpaceDto> childrens;
 
     public void setName(String name) {
         Preconditions.checkNotNull(name, ModelValidationError.WORKSPACE_NAME_NULL);
         this.name = name;
+    }
+
+    public Long getIdParent() {
+        return this.parent != null ? this.parent.getId() : null;
+    }
+
+    public String getNameParent() {
+        return this.parent != null ? this.parent.getName() : null;
     }
 }

32b30b55d937ff8ecfcbbde07e82c5072d4467ed
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2021-05-25 22:16:34 -0500

    Espacios de trabajos sin relaciones de capas

--- src/main/java/co/gov/corpocaldas/AccessLayerRequest/repository/WorkSpaceRepository.java
+++ src/main/java/co/gov/corpocaldas/AccessLayerRequest/repository/WorkSpaceRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface WorkSpaceRepository extends JpaRepository<WorkSpace, Long> {
 
     @Query("SELECT DISTINCT w FROM WorkSpace w INNER JOIN w.childrens c WHERE (:name IS NULL OR w.name LIKE %:name%)" +
-            " AND (:name IS NULL OR c.name LIKE %:name%) AND w.parent IS NULL")
+            " OR (:name IS NULL OR c.name LIKE %:name%) AND w.parent IS NULL")
     Page<WorkSpace> getAll(String name, Pageable pageable);
 
 }

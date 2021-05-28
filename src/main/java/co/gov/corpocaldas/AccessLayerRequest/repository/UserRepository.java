c10c0aad6d568ffc20d97fe7870eb79946efd129
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2021-05-27 21:01:49 -0500

    Añadiendo login

--- src/main/java/co/gov/corpocaldas/AccessLayerRequest/repository/UserRepository.java
+++ src/main/java/co/gov/corpocaldas/AccessLayerRequest/repository/UserRepository.java
@@ -18,7 +18,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query("SELECT u FROM User u WHERE (:name IS NULL OR u.name LIKE %:name%)" +
             " AND (:lastname IS NULL OR u.lastname LIKE %:lastname%) AND (:email IS NULL OR u.email LIKE %:email%)" +
-            " AND (:username IS NULL OR u.username LIKE %:username) AND (:isEnabled IS NULL OR u.enabled = :isEnabled")
+            " AND (:username IS NULL OR u.username LIKE %:username) AND (:isEnabled IS NULL OR u.enabled = :isEnabled)")
     Page<User> getAll(@Param("name") String name, @Param("lastname") String lastname, @Param("email") String email,
                       @Param("username") String username, @Param("isEnabled") Boolean isEnabled, Pageable pageable);
 
